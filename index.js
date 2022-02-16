23be0fd172c07a992f4963b9f1050ef79a195645
hermtechs <hermtechs@gmail.com>
2022-02-17 01:58:08 +0300

    navigation menu hamburger

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const scrollRightBtn = document.querySelector('.scroll-right')
 const scrollLeftBtn = document.querySelector('.scroll-left')
 const destinationContainer = document.querySelector('.places')
 const destinations  = document.querySelectorAll('.place');
+const navBtn = document.querySelector('.nav-toggle');
 
+
+// NAVIGATION
+navBtn.addEventListener('click', hamburgerOpenandClose)
+function hamburgerOpenandClose(){
+  if(navBtn.classList.contains('hamburger-open')==false){
+    navBtn.classList.add('hamburger-open');
+  }
+  else{
+  navBtn.classList.remove('hamburger-open');
+  }
+}
+
+//AUTO-SCROLLING AND SCROLL BUTTONS ON DESITINATIONS SECTION
 scrollRightBtn.addEventListener('click', scrollRight);
 scrollLeftBtn.addEventListener('click',scrollLeft)
 const elementSize = destinations[0].clientWidth
 console.log(elementSize)
-
-
 //FINDING TOTAL WIDTH COVERED BY PLACE ELEMENTS 
 var totalClientWidth = 0
 const totalElementsClientWidth = destinations.forEach((destination)=>{
